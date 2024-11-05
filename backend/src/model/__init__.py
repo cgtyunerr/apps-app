0a66d8def531341ddfb0bdd0ad73e2937ded2209
cgtyunerr <110602817+cgtyunerr@users.noreply.github.com>
2024-11-06 01:19:13 +0300

    add mixins and types

--- backend/src/model/__init__.py
+++ backend/src/model/__init__.py
@@ -1,1 +1,14 @@
 """Database model modules."""
+from .asset import Asset
+from .base import Base
+from .campaign import Campaign
+from .campaign_creative import CampaignCreative
+from .insight import Insight
+
+__all__ = [
+    "Base",
+    "Asset",
+    "Campaign",
+    "CampaignCreative",
+    "Insight",
+]
---
+++ backend/src/model/mixin.py
@@ -0,0 +1,71 @@
+"""Common mixins."""
+
+from datetime import datetime
+from typing import Annotated
+
+from sqlalchemy import BIGINT, Boolean, DateTime, String, Enum, REAL
+from sqlalchemy.orm import Mapped, declarative_mixin, declared_attr, mapped_column
+
+
+# Types
+item_id = Annotated[
+    int,
+    mapped_column(
+        BIGINT,
+        primary_key=True,
+        autoincrement=True,
+    )
+]
+
+integer = Annotated[
+    int,
+    mapped_column(
+        BIGINT,
+        nullable=False,
+    )
+]
+
+unique_int = Annotated[
+    int,
+    mapped_column(
+        BIGINT,
+        nullable=False,
+        unique=True,
+    )
+]
+
+real = Annotated[
+    float,
+    mapped_column(
+        REAL,
+        nullable=False,
+    )
+]
+
+boolean = Annotated[bool, mapped_column(Boolean, default=True, nullable=False)]
+
+text = Annotated[str, mapped_column(String, nullable=False)]
+
+
+class IdMixin:
+    """Mixin for id."""
+    id: Mapped[item_id]
+
+class IsActiveMixin:
+    """Mixin for is_active."""
+    is_active: Mapped[boolean]
+
+class CreatedAtMixin:
+    """Mixin for created_at."""
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True),
+        nullable=False,
+    )
+
+@declarative_mixin
+class TableNameMixin:
+    """Mixin for table_name."""
+    @declared_attr
+    def __tablename__(cls) -> str:
+        """Generate table name."""
+        return cls.__name__.lower()
