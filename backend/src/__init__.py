2fe47aaea18b3cc8e562cd16b830d4f8d11aff58
cgtyunerr <110602817+cgtyunerr@users.noreply.github.com>
2024-11-07 05:54:41 +0300

    add main

--- backend/src/__init__.py
+++ backend/src/__init__.py
@@ -1,1 +1,8 @@
 """Main project module."""
+from .core import settings
+from .model import Base
+
+__all__ = [
+    "Base",
+    "settings",
+]
--- backend/src/main.py
+++ backend/src/main.py
@@ -1,1 +1,13 @@
 """Main project file."""
+from fastapi import FastAPI
+from fastapi.middleware import Middleware
+
+from src.middleware import ErrorHandlerMiddleware
+
+app = FastAPI(
+    title="Backend",
+    description="Apps app backend",
+    version="1.0.0",
+)
+
+app.add_middleware(ErrorHandlerMiddleware)
