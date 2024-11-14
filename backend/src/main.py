c5043f05dd0aa4f286b46057790f496d1167c167
cgtyunerr <cgtyuner06@gmail.com>
2024-11-14 07:34:32 +0300

    add cors middleware

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,5 +1,6 @@
 """Main project file."""
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from src.api import asset_router, campaign_router, campaign_creative_router, insight_router
 from src.middleware import ErrorHandlerMiddleware
@@ -8,6 +9,14 @@ app = FastAPI(
     title="Backend",
     description="Apps app backend",
     version="1.0.0",
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 app.add_middleware(ErrorHandlerMiddleware)
