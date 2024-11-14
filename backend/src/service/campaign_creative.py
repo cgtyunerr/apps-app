860fbb57088bc1e6f14135be5ea2de3e615a6ccc
cgtyunerr <cgtyuner06@gmail.com>
2024-11-14 07:38:27 +0300

    fixup! add campaign creative service and api

--- backend/src/service/campaign_creative.py
+++ backend/src/service/campaign_creative.py
@@ -4,9 +4,9 @@ from sqlalchemy import delete, select
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from .service import Service
-from src.core import NotFoundError
+from src.core import InvalidInputError
 from src.domain import CampaignCreativeApiCall
-from src.model import CampaignCreative
+from src.model import CampaignCreative, Asset
 from src.schema import(
     ServiceCampaignCreativesSchema,
     ServiceCampaignCreativeSchema
@@ -56,8 +56,6 @@ class CampaignCreativeService(Service):
                 )
             )
 
-        if len(result) == 0:
-            raise NotFoundError
         return result
 
     @validate_call
@@ -76,28 +74,45 @@ class CampaignCreativeService(Service):
 
         Returns:
             ServiceCampaignCreativesSchema.
+
+        Raises:
+            InvalidInputError
         """
+        query_result = await session.execute(
+            select(Asset)
+            .filter(Asset.id.in_(asset_ids))
+            .filter(Asset.status == "eligible")
+        )
+        assets = query_result.scalars().all()
+        if len(assets) < len(asset_ids):
+            raise InvalidInputError("Some eligible assets not found.")
+
         result: ServiceCampaignCreativesSchema = campaign_creative_api_call.assign_campaign_creative(
             campaign_id=campaign_id,
-            asset_ids=asset_ids,
+            asset_ids=[asset.service_asset_id for asset in assets],
         )
-        if len(result) == 0:
-            raise NotFoundError
+        service_to_asset: dict[int, int] = {}
+        for asset in assets:
+            print("111")
+            service_to_asset[asset.service_asset_id] = asset.id
 
         new_campaign_creative: list[CampaignCreative] = []
         for campaign_creative in result:
+            print("s:", campaign_creative.asset_id)
+            print("d:", service_to_asset.keys())
+            print("r:", service_to_asset.values())
             new_campaign_creative.append(
                 CampaignCreative(
                     id=campaign_creative.id,
                     status=campaign_creative.status,
                     created_at=campaign_creative.created_at,
-                    asset_id=campaign_creative.asset_id,
+                    asset_id=service_to_asset[campaign_creative.asset_id],
                     campaign_id=campaign_id,
                 )
             )
-        session.add_all(new_campaign_creative)
-        await session.commit()
-        return result
+            session.add_all(new_campaign_creative)
+            await session.commit()
+            return result
 
     @validate_call
     async def unassign_campaign_creative(
