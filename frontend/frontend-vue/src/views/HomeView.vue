eaf14f0857be93dadb58f6f56a8396debded5aec
cgtyunerr <cgtyuner06@gmail.com>
2024-11-14 07:44:35 +0300

    add home view

---
+++ frontend/frontend-vue/src/views/HomeView.vue
@@ -0,0 +1,7 @@
+<template>
+  <b-container class="text-center">
+    <h1 class="my-5">Welcome to the Home Page</h1>
+    <RouterLink to="/campaign" class="mx-3">Campaign Page</RouterLink>
+    <RouterLink to="/asset" class="mx-3">Asset Page</RouterLink>
+  </b-container>
+</template>
